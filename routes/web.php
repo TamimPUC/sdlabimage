4a9f6d0549386e7062cc398779ce32925d7f12be
TamimPUC <tk390665@gmail.com>
2024-08-27 21:21:43 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
  
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController; //add the ControllerNameSpace
+use App\Http\Controllers\StudentController;
  
 Route::get('/', function () {
     return view('welcome');
+Route::resource("/employee", EmployeeController::class);
+Route::resource("/student", StudentController::class);
+Route::controller(StripePaymentController::class)->group(function()
+
+
+Route::get('stripe', 'stripe');
+    Route::post('stripe', 'stripePost')->name('stripe.post');
 });
  
-Route::resource("/employee", EmployeeController::class);
